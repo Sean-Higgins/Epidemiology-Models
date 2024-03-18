2d743b9ecfbb2fd5bed510149f5446f9e59e1393
Sean Higgins <seanhiggins639@gmail.com>
2024-03-17 19:57:28 -0700

    Updated influenzaOutbreak.cpp to accept user provided values on the command-line. Also added error checking for the user provided values to make sure they are valid.

--- influenzaOutbreak.cpp
+++ influenzaOutbreak.cpp
@@ -7,6 +7,8 @@
  * Start Date: September 15, 2023
  */
 
+#include <errno.h>
+#include <limits.h>
 #include <stdio.h>
 #define _USE_MATH_DEFINES
 #include <math.h>
@@ -16,22 +18,22 @@
 
 // print debugging messages?
 #ifndef DEBUG
-#define DEBUG	false
+#define DEBUG   false
 #endif
 
 // setting the number of threads:
 #ifndef NUMT
-#define NUMT	4
+#define NUMT    4
 #endif
 
 
 // Define the start and end year for the simulation.
 #ifndef START_YEAR
-#define START_YEAR	2023
+#define START_YEAR  2023
 #endif
 
 #ifndef END_YEAR
-#define END_YEAR	2029
+#define END_YEAR    2024
 #endif
 
 //#define CSV
@@ -45,29 +47,30 @@ int NowYear = START_YEAR;	// [START_YEAR, END_YEAR]
 int NowMonth = 0;			// [0, 11]
 
 // Starting number of susceptible people, infected people, and recovered people.
-int CurrentInfected = 10;
-int CurrentSusceptible = 175000 - CurrentInfected;
-int	CurrentRecovered = 0;
+long CurrentInfected = 10;
+long CurrentSusceptible = 175000;   // - CurrentInfected; The CurrentInfected is
+                                    // subtracted in main
+long CurrentRecovered = 0;
 
 // Transfer rates for the zombie outbreak model.
 // Rate of infection for the common cold.
-float InfectionRate = 0.4;
+double InfectionRate = 0.4;
 // Rate of recovery for the common cold.
-float RecoveryRate = 0.04;
+double RecoveryRate = 0.04;
 
 
 // For use with calculating the Carrying Capacity
-float Sqr( float x )
+double Sqr( double x )
 {
-        return x*x;
+    return x*x;
 }
 
 // Seed the random number function.
 unsigned int seed = 0;
 
 float Randf(unsigned int *seedp, float low, float high) {
-	float r = (float) rand_r(seedp);
-	return (low + r*(high - low) / (float)RAND_MAX);
+    float r = (float) rand_r(seedp);
+    return (low + r*(high - low) / (float)RAND_MAX);
 }
 
 
@@ -75,34 +78,31 @@ float Randf(unsigned int *seedp, float low, float high) {
 // there will be for the next generation of the simulation. This rate of
 // decrease is proportional to the current population zombies.
 void Susceptible() {
-	int nextSusceptible;
+    long nextSusceptible;
+
+    while( NowYear < END_YEAR ) {
+    	// compute a temporary next-value for this quantity
+    	// based on the current state of the simulation:
+    	nextSusceptible = CurrentSusceptible;
+
+    	// Subtract the number of new infections based on the current number
+        // of infected individuals.
+    	nextSusceptible -= CurrentInfected * InfectionRate;
 	
-	while( NowYear < END_YEAR ) {
-		// compute a temporary next-value for this quantity
-		// based on the current state of the simulation:
-		nextSusceptible = CurrentSusceptible;
-
-
-		// Subtract the number of new infections based on the current number
-		// of infected individuals.
-		nextSusceptible -= CurrentInfected * InfectionRate;
+        // We can't have a negative population
+    	if( nextSusceptible < 0 )
+            nextSusceptible = 0;
 		
-		// We can't have a negative population
-		if( nextSusceptible < 0 )
-			nextSusceptible = 0;
-		
-		// DoneComputing barrier:
-		#pragma omp barrier
-		CurrentSusceptible = nextSusceptible;
+    	// DoneComputing barrier:
+    	#pragma omp barrier
+    	CurrentSusceptible = nextSusceptible;
 
-		// DoneAssigning barrier:
-		#pragma omp barrier
-		
+    	// DoneAssigning barrier:
+    	#pragma omp barrier		
 
-		// DonePrinting barrier:
-		#pragma omp barrier
-		
-	}
+    	// DonePrinting barrier:
+    	#pragma omp barrier		
+    }
 }
 
 // This function calculates the number of infected individuals there will be
@@ -110,34 +110,32 @@ void Susceptible() {
 // susceptible individuals available to be infected, as well as the number of
 // infected individuals that have recovered.
 void Infected() {
-	int nextInfected;
+    long nextInfected;
 	
-	while( NowYear < END_YEAR ) {
-		// compute a temporary next-value for this quantity
-		// based on the current state of the simulation:
-		nextInfected = CurrentInfected;
-		
+    while( NowYear < END_YEAR ) {
+        // compute a temporary next-value for this quantity
+    	// based on the current state of the simulation:
+    	nextInfected = CurrentInfected;
 
-		// Add the new number of infected indivuduals and subtract
-		// the number of recovered individuals.
-		nextInfected += (CurrentSusceptible * InfectionRate);
-		nextInfected -= (CurrentInfected * RecoveryRate);
-		
-		// We still cannot have a negative population
-		if (nextInfected < 0)
-			nextInfected = 0;
+    	// Add the new number of infected indivuduals and subtract
+        // the number of recovered individuals.
+        nextInfected += (CurrentSusceptible * InfectionRate);
+        nextInfected -= (CurrentInfected * RecoveryRate);
 
-		// DoneComputing barrier:
-		#pragma omp barrier
-		CurrentInfected = nextInfected;
+    	// We still cannot have a negative population
+	if (nextInfected < 0)
+            nextInfected = 0;
 
-		// DoneAssigning barrier:
-		#pragma omp barrier
-		
+    	// DoneComputing barrier:
+    	#pragma omp barrier
+    	CurrentInfected = nextInfected;
 
-		// DonePrinting barrier:
-		#pragma omp barrier
-	}
+    	// DoneAssigning barrier:
+    	#pragma omp barrier
+
+    	// DonePrinting barrier:
+    	#pragma omp barrier
+    }
 }
 
 
@@ -145,111 +143,259 @@ void Infected() {
 // their flu infection. This depends on the number of people who are currently
 // infected.
 void Recovered() {
-	int nextRecovered = CurrentRecovered;
-	
-	while( NowYear < END_YEAR ) {
-		// Compute a temporary next-value for the number of recovered individuals
-		// based on the current number of infected individuals.
+    long nextRecovered = CurrentRecovered;
 
-		
-		nextRecovered += (CurrentInfected * RecoveryRate);
-		
-		// DoneComputing barrier: Save the calculated variables to the
-		// global variables.
-		#pragma omp barrier
-		CurrentRecovered = nextRecovered;
+    while( NowYear < END_YEAR ) {
+	// Compute a temporary next-value for the number of recovered individuals
+        // based on the current number of infected individuals.
 
-		// DoneAssigning barrier:
-		#pragma omp barrier
+    	nextRecovered += (CurrentInfected * RecoveryRate);
 		
+    	// DoneComputing barrier: Save the calculated variables to the
+    	// global variables.
+    	#pragma omp barrier
+    	CurrentRecovered = nextRecovered;
 
-		// DonePrinting barrier:
-		#pragma omp barrier
-		
-	}
+    	// DoneAssigning barrier:
+    	#pragma omp barrier
+
+    	// DonePrinting barrier:
+    	#pragma omp barrier		
+    }
 }
 
 // This function adjusts the global variables for the program.
 void Watcher() {
-	int tempMonth;
-	int tempYear;
+    int tempMonth;
+    int tempYear;
+
+    while( NowYear < END_YEAR ) {
+
+	// DoneComputing barrier:
+	#pragma omp barrier
+
+	// DoneAssigning barrier:
+	#pragma omp barrier
+
+	// Print the current values for the simulation.
+	#ifdef CSV
+	    // Calculate the current month number for graphing purposes.
+	    int yearDiff = NowYear - START_YEAR;
+            int addMonths = 12*yearDiff;
+	    int printMonth = NowMonth+addMonths;
+
+            fprintf(stderr, "%2d, %ld, %ld, %ld\n", printMonth, CurrentSusceptible, CurrentInfected, CurrentRecovered);
 	
-	while( NowYear < END_YEAR ) {
+        #else
+            fprintf(stderr, "Year %4d, Month %2d - Susceptible: %6ld, Infected: %6ld, Recovered: %6ld\n", NowYear, NowMonth+1, CurrentSusceptible, CurrentInfected, CurrentRecovered);
+        #endif
+	
+	// Compute a temporary next-value for this quantity
+	// based on the current state of the simulation:
+        tempYear = NowYear;
+	tempMonth = NowMonth + 1;
 
-		// DoneComputing barrier:
-		#pragma omp barrier
+	if (tempMonth > 11) {
+            tempMonth = 0;
+	    tempYear++;
+        }
 
+        // Store the new environment variables for the simulation.
+        NowMonth = tempMonth;
+	NowYear = tempYear;
 
-		// DoneAssigning barrier:
-		#pragma omp barrier
-		
-		// Print the current values for the simulation.
-		#ifdef CSV
-			// Calculate the current month number for graphing purposes.
-			int yearDiff = NowYear - START_YEAR;
-			int addMonths = 12*yearDiff;
-			int printMonth = NowMonth+addMonths;
-			
-			fprintf(stderr, "%2d, %d, %d, %d\n",
-					printMonth, CurrentSusceptible, CurrentInfected, CurrentRecovered);
-		#else
-			fprintf(stderr, "Year %4d, Month %2d - Susceptible: %d, Infected: %d, Recovered: %d\n",
-					NowYear, NowMonth+1, CurrentSusceptible, CurrentInfected, CurrentRecovered);
-		#endif
-		
-		// Compute a temporary next-value for this quantity
-		// based on the current state of the simulation:
-		tempYear = NowYear;
-		tempMonth = NowMonth + 1;
-		
-		if (tempMonth > 11) {
-			tempMonth = 0;
-			tempYear++;
-		}
-		
-		// Store the new environment variables for the simulation.
-		NowMonth = tempMonth;
-		NowYear = tempYear;
-		
-		// DonePrinting barrier:
-		#pragma omp barrier
-	}
+	// DonePrinting barrier:
+	#pragma omp barrier
+    }
 }
 
 // The main loop of the program.
 int main(int argc, char* argv[]) {
-        // Gather the initial values from the command-line
-        if (argc >= 2) {
-            CurrentSusceptible = atoi(argv[1]);
-            CurrentInfected = atoi(argv[2]);
+    // Gather the initial values (if any) from the command-line
+    // If command-line arguments are provided, they are parsed in with
+    // the appropriate flags:
+    //  -
+    if (argc >= 2) {
+        // Parse all of the provided arguments and their values.
+        // IMPORTANT: We also need to handle error cases where a user
+        // forgets to inter a value (e.g., ./influenzaOutbreak -s)
+        // This leaves argv[argc-1] as the flag, and the final argv[argc] as NULL.
+
+        // Iterate through the argument vector to locate the flags.
+        int c, errno;
+        char *endptr, *str;
+        double double_val;  // Store values from strtod() (i.e., the rates)
+
+        for (int i = 1; i < argc; ++i) {
+            // If the current argument is an option, determine which option
+            // and store the provided value to the corresponding variable
+            if (argv[i][0] == '-') {
+                c = argv[i][1]; // Get the option character
+
+                errno = 0;      // Distinguish between a success and a failure.
+                str = argv[++i];    // Store the pointer that holds the number value
+                                    // to convert.
+                switch (c) {
+                    case 's':   // -s: Initial susceptible value. Value must be a
+                                // whole positive integer.
+                        CurrentSusceptible = strtol(str, &endptr, 0); // Store the provided value.
+
+                        // Error checking. If errno is not 0, something went wrong
+                        // with strtol()
+                        if (errno != 0) {
+                            perror("strtol");
+                            exit(EXIT_FAILURE);
+                        }
+
+                        // The stored value of endptr points to the end of the
+                        // numeric section of str, so if str == endptr, that means
+                        // there was never a numeric section to str!
+                        if (str == endptr) {
+                            fprintf(stderr, "No digits provided for the initial susceptible value.\n");
+                            exit(EXIT_FAILURE);
+                        }
+
+                        // Finally, we need to make sure that the provided value is
+                        // not less than 0.
+                        if (CurrentSusceptible < 0) {
+                            fprintf(stderr, "The provided value of %ld is invalid\n",
+                                    CurrentSusceptible);
+                            exit(EXIT_FAILURE);
+                        }
+
+                        // If everything looks good however, we can store the
+                        // provided value for the initial susceptible amount.
+                        break;
+                    case 'i':   // -i: Initial infected value. Value must be a whole
+                                // positive integer.
+                        CurrentInfected = strtol(str, &endptr, 0); // Store the provided value.
+
+                        // Error checking. If errno is not 0, something went wrong
+                        // with strtol()
+                        if (errno != 0) {
+                            perror("strtol");
+                            exit(EXIT_FAILURE);
+                        }
+
+                        // The stored value of endptr points to the end of the
+                        // numeric section of str, so if str == endptr, that means
+                        // there was never a numeric section to str!
+                        if (str == endptr) {
+                            fprintf(stderr, "No digits provided for the initial infected value.\n");
+                            exit(EXIT_FAILURE);
+                        }
+
+                        // Finally, we need to make sure that the provided value is
+                        // not less than 0.
+                        if (CurrentInfected < 0) {
+                            fprintf(stderr, "The provided value of %ld is invalid\n",
+                                    CurrentInfected);
+                            exit(EXIT_FAILURE);
+                        }
+
+                        // If everything looks good however, we can store the
+                        // provided value for the initial infected amount.
+                        break;
+                    case 'a':   // -a: Rate of infection (from susceptible to
+                                //      infected). Must be a non-negative value.
+                        InfectionRate = strtod(str, &endptr);
+
+                        // Error checking. If errno is not 0, then something went
+                        // wrong with strtod()
+                        if (errno != 0) {
+                            perror("strtod");
+                            exit(EXIT_FAILURE);
+                        }
+
+                        // The stored value of endptr points to the end of the
+                        // numeric section of str, so if str == endptr, that means
+                        // there was never a numeric section to str
+                        if (str == endptr) {
+                            fprintf(stderr, "No digits provided for the infection rate value.\n");
+                            exit(EXIT_FAILURE);
+                        }
+
+                        // Finally, we need to make sure that the provided value is
+                        // not less than 0.
+                        if (InfectionRate < 0) {
+                            fprintf(stderr, "The provided value of %f is invalid.\n",
+                                    InfectionRate);
+                            exit(EXIT_FAILURE);
+                        }
+
+                        // If everything looks good however, we can store the
+                        // provided value for the infection rate.
+                        break;
+                    case 'r':   // -r: Rate of recovery (from infected to recovered).
+                                // Must be a non-negative value.
+                        RecoveryRate = strtod(str, &endptr);
+
+                        // Error checking. If errno is not 0, then something went
+                        // wrong with strtod()
+                        if (errno != 0) {
+                            perror("strtod");
+                            exit(EXIT_FAILURE);
+                        }
+
+                        // The stored value of endptr points to the end of the
+                        // numeric section of str, so if str == endptr, that means
+                        // there was never a numeric section to str
+                        if (str == endptr) {
+                            fprintf(stderr, "No digits provided for the infection rate value.\n");
+                            exit(EXIT_FAILURE);
+                        }
+
+                        // Finally, we need to make sure that the provided value is
+                        // not less than 0.
+                        if (RecoveryRate < 0) {
+                            fprintf(stderr, "The provided value of %f is invalid.\n",
+                                    RecoveryRate);
+                            exit(EXIT_FAILURE);
+                        }
+
+                        // If everything looks good however, we can store the
+                        // provided value for the infection rate.
+                        break;
+                    default:    // If the provided argument flag is invalid, print a
+                                // message explaining the flag is invalid. Then print
+                                // the usage message.
+                        fprintf(stderr, "Error: The provided flag of %s is invalid.\n", argv[--i]);
+                        fprintf(stderr, "Usage: %s [-s susceptible] [-i infected] [-a rate-of-infection] [-r rate-of-recovery]\n",
+                                 argv[0]);
+                        exit(EXIT_FAILURE);
+                }
+
+                // Once the user-provided values have been parsed (if they were
+                // provided), we need to subtract the number of infected from the
+                // beginning population of CurrentSusceptible
+                CurrentSusceptible -= CurrentInfected;
+            }
         }
-        if (argc >= 4) {
-            
+    }
+
+    omp_set_num_threads(NUMT);	// same as # of sections
+    #pragma omp parallel sections
+    {
+        #pragma omp section
+        {
+	    Susceptible();
         }
 
-	omp_set_num_threads(NUMT);	// same as # of sections
-	#pragma omp parallel sections
+	#pragma omp section
 	{
-		#pragma omp section
-		{
-			Susceptible();
-		}
-
-		#pragma omp section
-		{
-			Infected();
-		}
+            Infected();
+	}
 		
-		#pragma omp section
-		{
-			Recovered();
-		}
+	#pragma omp section
+	{
+            Recovered();
+	}
 		
-		#pragma omp section
-		{
-			Watcher();
-		}
+	#pragma omp section
+	{
+            Watcher();
+	}
 		
-	}   // implied barrier -- all functions must return in order
-		// to allow any of them to get past here
+    }   // implied barrier -- all functions must return in order
+	// to allow any of them to get past here
 }
