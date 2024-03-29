1eda4f6a817f44948dc0a65b3afe0e4d3ed45e50
Sean Higgins <seanhiggins639@gmail.com>
2024-03-29 15:19:21 -0700

    Beginning to implement the SQEIR model to model the COVID-19 virus.

---
+++ SQEIR Model/SQEIR.cpp
@@ -0,0 +1,144 @@
+/*
+ * Program Name: influenzaSIR.cpp
+ * Summary: This program uses multithreading and parallel calculations
+ *          to simulate the progress of a flu outbreak on a human population
+ *          using the SIR epidemiology model.
+ *              (S)usceptible
+ *              (I)nfected
+ *              (R)ecovered
+ * Programmer: Sean B. Higgins
+ * Start Date: September 15, 2023
+ */
+
+#include <errno.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <omp.h>
+
+#include "SQEIR.h"
+
+
+// The main loop of the program.
+int main(int argc, char* argv[]) {
+    // Gather the initial values (if any) from the command-line
+    // If command-line arguments are provided, they are parsed in with
+    // the appropriate flags:
+    //  -
+    if (argc >= 2) {
+        // Parse all of the provided arguments and their values.
+        // IMPORTANT: We also need to handle error cases where a user
+        // forgets to inter a value (e.g., ./influenzaOutbreak -s)
+        // This leaves argv[argc-1] as the flag, and the final argv[argc] as NULL.
+
+        // Iterate through the argument vector to locate the flags.
+        int c;
+        char *endptr, *str;
+        double double_val;  // Store values from strtod() (i.e., the rates)
+
+        for (int i = 1; i < argc; ++i) {
+            // If the current argument is an option, determine which option
+            // and store the provided value to the corresponding variable
+            if (argv[i][0] == '-') {
+                c = argv[i][1]; // Get the option character
+
+                str = argv[++i];    // Store the pointer that holds the number value
+                                    // to convert.
+
+                // IMPORTANT: If the user didn't actually provide an argument after
+                // one of the argument flags, that will result in str being given a
+                // nullptr. If we try to run strtol() or strtod() on a nullptr, we
+                // get a segfault. 
+                if (str == nullptr) {
+                    fprintf(stderr, "Error: No argument was provided for option flag \"%s\".\n", argv[--i]);
+                    fprintf(stderr, "Usage: %s [-s susceptible] [-i infected] [-b rate-of-infection] [-g rate-of-recovery]\n",
+                        argv[0]);
+                    exit(EXIT_FAILURE);
+ 
+                }
+                
+                switch (c) {
+                    case 's':   // -s: Initial susceptible value. Value must be a
+                                // whole positive integer.
+                        parseLong(&CurrentSusceptible, str);
+                        break;
+                    case 'i':   // -i: Initial infected value. Value must be a whole
+                                // positive integer.
+                        parseLong(&CurrentInfected, str);
+                        break;
+                    case 'b':   // -b: Rate of infection (from susceptible to
+                                //      infected). Must be a non-negative value.
+                        parseDouble(&InfectionRate, str);
+                        break;
+                    case 'g':   // -g: Rate of recovery (from infected to recovered).
+                                // Must be a non-negative value.
+                        parseDouble(&RecoveryRate, str);
+                        break;
+                    default:    // If the provided argument flag is invalid, print a
+                                // message explaining the flag is invalid. Then print
+                                // the usage message.
+                        fprintf(stderr, "Error: The provided flag of \"%s\" is invalid.\n", argv[--i]);
+                        fprintf(stderr, "Usage: %s [-s susceptible] [-i infected] [-b rate-of-infection] [-g rate-of-recovery]\n",
+                                 argv[0]);
+                        exit(EXIT_FAILURE);
+                }
+            }
+        }
+    }
+
+    // Once the user-provided values have been parsed (if they were
+    // provided), we need to subtract the number of infected from the
+    // beginning population of CurrentSusceptible
+    CurrentSusceptible -= CurrentInfected;
+
+    // IMPORTANT: Before we begin our calculations, we need to print out the initial
+    // population values.
+#ifdef CSV
+    // Calculate the current month number for graphing purposes.
+    int yearDiff = NowYear - START_YEAR;
+    int addMonths = 12*yearDiff;
+    int printMonth = NowMonth+addMonths;
+
+    fprintf(stderr, "%2d, %ld, %ld, %ld\n", printMonth, CurrentSusceptible, CurrentInfected, CurrentRecovered);
+#else
+    fprintf(stderr, "Year %4d, Month %2d - Susceptible: %6ld, Infected: %6ld, Recovered: %6ld\n",
+            NowYear, NowMonth+1, CurrentSusceptible, CurrentInfected, CurrentRecovered);
+#endif
+
+    // For debugging, print the total population as we go.
+#ifdef DEBUG
+    fprintf(stderr, "Total Population: %6ld\n",
+            CurrentSusceptible + CurrentInfected + CurrentRecovered);
+#endif
+
+    // Increment to the next month to begin our calculations.
+    NowMonth++;
+
+    omp_set_num_threads(NUMT);	// same as # of sections
+    #pragma omp parallel sections
+    {
+        #pragma omp section
+        {
+	    Susceptible();
+        }
+
+	#pragma omp section
+	{
+            Infected();
+	}
+		
+	#pragma omp section
+	{
+            Recovered();
+	}
+		
+	#pragma omp section
+	{
+            Watcher();
+	}
+		
+    }   // implied barrier -- all functions must return in order
+	// to allow any of them to get past here
+}
---
+++ SQEIR Model/SQEIR.h
@@ -0,0 +1,75 @@
+/*
+ * SQEIR.h - A header file for the SQEIR.cpp program to store
+ *	     the declarations and definitions of the most
+ *           important funcations and variables for the program.
+ */
+
+// print debugging messages?
+//#define DEBUG
+
+// setting the number of threads:
+#ifndef NUMT
+#define NUMT    6
+#endif
+
+
+// Define the start and end year for the simulation.
+#ifndef START_YEAR
+#define START_YEAR  2023
+#endif
+
+#ifndef END_YEAR
+#define END_YEAR    2025
+#endif
+
+//#define CSV
+
+// Since the below variables are shared between influenzaSIR.cpp and SIR.c,
+// we need to declare them as extern variable so that they can be used in all the
+// files that make up this program. The variables are declared here in the header
+// file, are assigned their default values in the SIR.c file, and can be accessed
+// and changed in the influenzaSIR.cpp file.
+
+// Year and month for the simulation to keep track of.
+extern int NowYear;	    // [START_YEAR, END_YEAR]
+extern int NowMonth;	// [0, 11]
+
+// Starting number of susceptible people, infected people, and recovered people.
+extern long CurrentSusceptible;
+extern long CurrentQuarantined;
+extern long CurrentExposed;
+extern long CurrentInfected;
+extern long CurrentRecovered;
+
+extern double InfectionRate;
+extern double RecoveryRate;
+
+// parseLong: This function serves as a wrapper for the function strtol(),
+//            ensuring that the provided character string str produces
+//            a long integer that meets our criteria.
+//  Input:
+//      long* long_val: A pointer to the variable where we will be storing our
+//                      converted str.
+//      char* str:      The string that holds the numeric data to convert to a
+//                      long integer
+void parseLong(long* long_val, char* str);
+
+
+// parseDouble: This function serves as a wrapper for the function strtod(),
+//              ensuring that the provided character string str produces
+//              a double that meets our criteria.
+//  Input:
+//      double* double_val: A pointer to the variable where we will be storing our
+//                      converted str.
+//      char* str:      The string that holds the numeric data to convert to a
+//                      double
+void parseDouble(double* double_val, char* str);
+
+
+// Function prototypes for the functions stored in the SIR.c file.
+void Susceptible();
+void Quarantined();
+void Exposed();
+void Infected();
+void Recovered();
+void Watcher();
