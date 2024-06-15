211e987425246bf57b0924a8bd89098447072dbd
Sean Higgins <seanhiggins639@gmail.com>
2024-06-15 10:04:59 -0700

    Implemented the OpenCL code needed for the matrix-vector multiplication operation to take place on the GPU device.

--- SIR Model/MatrixVectorMult.cl
+++ SIR Model/MatrixVectorMult.cl
@@ -6,7 +6,11 @@
 // x: A vector with n rows and 1 column.
 // b: The resultant vector with m rows and 1 column.
 
-kernel void MatrixVectorMult(IN global const long *dA, IN global const long *dx, IN global int *dMW, OUT global long *db) {
+kernel void MatrixVectorMult(IN global const float *dA, IN global const long *dx, IN global int *dMW, OUT global long *db) {
+    // [dA] is a matrix of dimensions m rows by dMW columns
+    // [dx] is a vector of dimensions dMW rows by 1 column
+    // [db] is a vector of dimensions m rows by 1 column
+    
     int mw = *dMW;
     int brow = get_global_id(0);
     int bcol = get_global_id(1);
@@ -15,10 +19,10 @@ kernel void MatrixVectorMult(IN global const long *dA, IN global const long *dx,
     int xindex = bcol;              // x[0][j]
     int bindex = brow * mw + bcol;  // b[i][j]
 
-    long bij = 0.0;
+    long bij = 0;
 
     for (int k = 0; k < mw; k++) {
-        bij += dA[aindex] * dx[xindex];
+        bij += round(dA[aindex] * dx[xindex]);
         aindex++;
         xindex += mw;
     }
--- SIR Model/SIR.cpp
+++ SIR Model/SIR.cpp
@@ -10,7 +10,8 @@
  * Start Date: September 15, 2023
  */
 
-// Implementing a matrix-vector multpiplication method to perform the
+// Implementing a matrix-vector multpiplication method to perform the calculations
+// for this epidemiological model.
 
 #include <errno.h>
 #include <limits.h>
@@ -56,12 +57,28 @@ cl_command_queue    CmdQueue;
 
 // Prepare the host matrix and vectors for calculating the linear system of equations:
 // Ax = b.
-long            hA[MATW][MATW];
-long            hx[MATW];
-long            hb[MATW];
 
-// Open the OpenCL file that has the function for matrix-vector multiplication.
-const char *    CL_FILE_NAME = {"MatrixVectorMult.cl"};
+/*
+ * dS/dt = -(InfectionRate)*Susceptible
+ * dI/dt = InfectionRate*Susceptible - RecoveryRate*Infected
+ * dR/dt = RecoveryRate*Infected
+ * 
+ * This system of linear equations can be converted to a matrix-vector multiplication:
+ * [-InfectionRate      0                   0][ Susceptible ]   [dS/dt]
+ * [InfectionRate       -RecoveryRate       0][ Infected    ] = [dI/dt]
+ * [0                   RecoveryRate        0][ Recovered   ]   [dR/dt]
+ * 
+ * Once the change in population has been calculated, it 
+ */
+
+double          hA[MATW][MATW];     // This matrix holds the linear equations for the population change
+long            hx[MATW];           // Holds population values for run n
+long            hb[MATW];           // Holds population values for run n+1
+
+// Open the OpenCL files that have the function for matrix-vector multiplication
+// and vector-addition.
+const char *    CL_FILE_NAME_1 = {"MatrixVectorMult.cl"};
+const char *    CL_FILE_NAME_2 = {"VectorAdd.cl"};
 
 // Function prototypes.
 void            SelectOpenclDevice();
@@ -76,25 +93,27 @@ int main(int argc, char* argv[]) {
     return 1;
 #endif
 
-    // Attempt to open the OpenCL kernel program. There's no point continuing if we can't
-    FILE *fp;
+    // Attempt to open the OpenCL kernel programs. There's no point continuing if we can't
+    FILE *fp1;
+    FILE *fp2;
 
 #ifdef WIN32
-    errno_t err = fopen_s( &fp, CL_FILE_NAME, "r");
-    if (err != 0)
+	errno_t err1 = fopen_s( &fp1, CL_FILE_NAME_1, "r" );
+	errno_t err2 = fopen_s( &fp2, CL_FILE_NAME_2, "r" );
+	if ( err1 != 0 || err2 != 0)
 #else
-    fp1 = fopen(CL_FILE_NAME, "r");
-    if (fp == NULL)
+	fp1 = fopen( CL_FILE_NAME_1, "r" );
+	fp2 = fopen( CL_FILE_NAME_2, "r" );
+	if ( fp1 == NULL || fp2 == NULL)
 #endif
-    {
-        fprintf(stderr, "Cannot open OpenCL source file '%s'\n", CL_FILE_NAME);
-        return 1;
-    }
+	{
+		fprintf( stderr, "Cannot open OpenCL source file '%s' or '%s'\n", CL_FILE_NAME_1, CL_FILE_NAME_2 );
+		return 1;
+	}
 
     // Gather the initial values (if any) from the command-line
     // If command-line arguments are provided, they are parsed in with
     // the appropriate flags:
-    //  -
     if (argc >= 2) {
         // Parse all of the provided arguments and their values.
         // IMPORTANT: We also need to handle error cases where a user
@@ -156,6 +175,16 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    // Returned status from OpenCL calls -- Test against CL_SUCCESS
+    cl_int status;
+
+    // Get the platform id and the device id:
+    SelectOpenclDevice();   // Sets the global variables Platform and Device
+
+    // 2. Allocate the host memory buffers:
+    // Already done. The host memory buffers were created as global variables instead of on the heap
+    // so that we could allocate them as a 2D array
+
     // Once the user-provided values have been parsed (if they were
     // provided), we need to subtract the number of infected from the
     // beginning population of CurrentSusceptible
@@ -183,6 +212,173 @@ int main(int argc, char* argv[]) {
 
     // Increment to the next month to begin our calculations.
     NowMonth++;
+
+    // Initialize the input matrices and vectors.
+    /*
+     * This system of linear equations can be converted to a matrix-vector multiplication:
+     * [-InfectionRate      0                   0][ Susceptible ]   [dS/dt]
+     * [InfectionRate       -RecoveryRate       0][ Infected    ] = [dI/dt]
+     * [0                   RecoveryRate        0][ Recovered   ]   [dR/dt]
+     */
+
+    // hA: Rates and linear system of equations
+    // Row 0: dS/dt
+    hA[0][0] = -InfectionRate;
+    
+    // Row 1: dI/dt
+    hA[1][0] = InfectionRate;
+    hA[1][1] = -RecoveryRate;
+    
+    // Row 2: dR/dt
+    hA[2][1] = RecoveryRate;
+
+    // hx: Population values
+    hx[0] = CurrentSusceptible;
+    hx[1] = CurrentInfected;
+    hx[2] = 0;    // No-one has recovered at the beginning of the simulation.
+
+    // 3. Create an OpenCL Context
+    Context = clCreateContext(NULL, 1, &Device, NULL, NULL, &status);
+    if (status != CL_SUCCESS)
+        fprintf(stderr, "clCreateContext failed\n");
+    
+    // 4. Create an OpenCL command queue:
+    CmdQueue = clCreateCommandQueue( Context, Device, 0, &status );
+	if( status != CL_SUCCESS )
+		fprintf( stderr, "clCreateCommandQueue failed\n" );
+    
+    // 5. Allocate the GPU device memory buffers for the A matrix and the x and b vectors
+    size_t aSize = MATW * MATW * sizeof(float);
+    size_t xSize = MATW * sizeof(long);
+    int mw = MATW;
+    size_t mwSize = sizeof(mw);
+    size_t bSize = MATW * sizeof(long);
+
+    // Allocating device memory for the A matrix
+    cl_mem dA = clCreateBuffer(Context, CL_MEM_READ_ONLY, aSize, NULL, &status);
+    if (status != CL_SUCCESS)
+        fprintf(stderr, "clCreateBuffer failed for dA (1)\n");
+    
+    // Allocating device memory for the x vector
+    cl_mem dx = clCreateBuffer(Context, CL_MEM_READ_ONLY, xSize, NULL, &status);
+    if (status != CL_SUCCESS)
+        fprintf(stderr, "clCreateBuffer failed for dx (1)\n");
+    
+    // Allocating device memory for the A matrix width/x vector height
+    cl_mem dMW = clCreateBuffer(Context, CL_MEM_READ_ONLY, mwSize, NULL, &status);
+    if (status != CL_SUCCESS)
+        fprintf(stderr, "clCreateBuffer failed for dMW (1)\n");
+    
+    // Allocating device memory for the b vector
+    cl_mem db = clCreateBuffer(Context, CL_MEM_WRITE_ONLY, bSize, NULL, &status);
+    if (status != CL_SUCCESS)
+        fprintf(stderr, "clCreateBuffer failed for dc (1)\n");
+    
+
+    // 6. Enqueue the 3 commands to write the data from the host buffers to the device buffers:
+
+    // Enqueue the data from matrix A to the device
+    status = clEnqueueWriteBuffer(CmdQueue, dA, CL_FALSE, 0, aSize, hA, 0, NULL, NULL);
+    if (status != CL_SUCCESS)
+        fprintf(stderr, "clEnqueueWriteBuffer failed for matrix A (1)\n");
+    
+    // Enqueue the data from vector x to the device
+    status = clEnqueueWriteBuffer(CmdQueue, dx, CL_FALSE, 0, xSize, hx, 0, NULL, NULL);
+    if (status != CL_SUCCESS)
+        fprintf(stderr, "clEnqueueWriteBuffer failed for vector x (1)\n");
+
+    // Enqueue the matrix width/vector height data dMW to the device
+    status = clEnqueueWriteBuffer(CmdQueue, dMW, CL_FALSE, 0, mwSize, hMW, 0, NULL, NULL);
+    if (status != CL_SUCCESS)
+        fprintf(stderr, "clEnqueueWriteBuffer failed for matrix width/vector height mw (1)\n");
+    
+    Wait(CmdQueue);
+
+    // This code is for the MatrixVectorMult and VectorAdd GPU parallelization functions.
+    // 7. Read the kernel code from the MatrixVectorMult.cl and VectorAdd.cl files...
+
+    // Read MatrixVectorMult.cl for MatrixVectorMult
+    fseek(fp1, 0, SEEK_END);
+    size_t fileSize = ftell(fp1);
+    fseek(fp1, 0, SEEK_SET);
+    char *clProgramTextMatVecMult = new char[ fileSize+1 ];     // Leave room for '\0'
+    size_t n = fread( clProgramTextMatVecMult, 1, fileSize, fp1);
+    clProgramTextMatVecMult[fileSize] = '\0';
+    fclose(fp1);
+    if (n != fileSize)
+        fprintf(stderr, "Expected to read %d bytes from '%s' -- actually read %d.\n", fileSize, CL_FILE_NAME_1, n);
+
+    // Read VectorAdd.cl for VectorAdd
+    fseek(fp2, 0, SEEK_END);
+    fileSize = ftell(fp2);
+    fseek(fp2, 0, SEEK_SET);
+    char *clProgramTextVecAdd = new char[ fileSize+1 ];     // Leave room for '\0'
+    n = fread( clProgramTextVecAdd, 1, fileSize, fp2);
+    clProgramTextVecAdd[fileSize] = '\0';
+    fclose(fp2);
+    if (n != fileSize)
+        fprintf(stderr, "Expected to read %d bytes from '%s' -- actually read %d.\n", fileSize, CL_FILE_NAME_2, n);
+
+    // ... and create the kernel program:
+
+    char *strings[2];
+    strings[0] = clProgramTextMatVecMult;   // Add both th MatrixVectorMult and VectorAdd kernels to the list of string pointers
+    strings[1] = clProgramTextVecAdd;       // for use with creating the program.
+    Program = clCreateProgramWithSource(Context, 2, (const char **)strings, NULL, &status); //IMPORTANT: Multiple kernel .cl files
+    if (status != CL_SUCCESS)
+        fprintf(stderr, "clCreateProgramWithSource failed\n");
+    delete [ ] clProgramTextMatVecMult;
+    delete [ ] clProgramTextVecAdd;
+
+
+    // 8. Compile and link the kernel code:
+    char *options = { (char*)"" };
+    status = clBuildProgram(Program, 1, &Device, options, NULL, NULL);
+    if (status != CL_SUCCESS) {
+        size_t size;
+        clGetProgramBuildInfo(Program, Device, CL_PROGRAM_BUILD_LOG, 0, NULL, &size);
+        cl_char *log = new cl_char[size];
+        clGetProgramBuildInfo(Program, Device, CL_PROGRAM_BUILD_LOG, size, log, NULL);
+        fprintf(stderr, "clBuildProgram failed:\n%s\n", log);
+        delete [ ] log;
+    }
+
+    
+    // 9. Create the kernel object (for MatrixVectorMult):
+
+    // Create a Kernel for the MatrixMult function.
+    Kernel = clCreateKernel(Program, "MatrixVectorMult", &status);
+    if (status != CL_SUCCESS)
+        fprintf(stderr, "clCreateKernel failed for MatrixVectorMult\n");
+    
+    // 10. Setup the arguments to the kernel object:
+
+    // Passing the dA matrix arguments to the kernal object.
+    status = clSetKernelArg(Kernel, 0, sizeof(cl_mem), &dA);
+    if (status != CL_SUCCESS)
+        fprintf(stderr, "clSetKernelArg failed for dA (%d)\n", status);
+    
+
+    // Passing the dx vector arguments to the kernal object.
+    status = clSetKernelArg(Kernel, 1, sizeof(cl_mem), &dx);
+    if (status != CL_SUCCESS)
+        fprintf(stderr, "clSetKernelArg failed for dx (%d)\n", status);
+
+    // Passing the dMW matrix width/vector height arguments to the kernal object.
+    status = clSetKernelArg(Kernel, 2, sizeof(cl_mem), &dMW);
+    if (status != CL_SUCCESS)
+        fprintf(stderr, "clSetKernelArg failed for dMW (%d)\n", status);
+
+    // Passing the db vector arguments to the kernal object.
+    status = clSetKernelArg(Kernel, 3, sizeof(cl_mem), &db);
+    if (status != CL_SUCCESS)
+        fprintf(stderr, "clSetKernelArg failed for dA (%d)\n", status);
+
+    // 11. Enqueue the kernel object for execution
+    size_t globalWorkSize[3] = {MATW,  MATW,   1};
+    size_t localWorkSize[3]  = {LOCALSIZE, LOCALSIZE, 1};
+
+
 
     omp_set_num_threads(NUMT);	// same as # of sections
     #pragma omp parallel sections
